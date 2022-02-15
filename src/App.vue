10a79dc5bc03e09a9fc908a530cd2d77e711841b
Hahalley <m222817735@gmail.com>
2022-02-16 00:02:41 +0800

    add NavBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div id="app">
+    <Navbar v-if="$route.path !== '/'" />
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from './components/NavBar.vue'
+
+export default {
+  created() {
+    console.log(this.$route.path)
+  },
+  components: {
+    Navbar,
+  },
+}
+</script>
 
 <style lang="scss"></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="tw-flex tw-bg-primary-100 tw-items-center tw-py-1">
+    <img
+      class="tw-w-14 tw-h-14 tw-pl-2"
+      src="../assets/icon/logo-white.png"
+      alt=""
+    />
+    <span class="tw-text-white tw-mr-auto tw-ml-3">活悅診所線上預約</span>
+    <a href="#" class="tw-w-1/10 tw-h-1/10 tw-pr-2"
+      ><img src="https://img.icons8.com/ios/250/FFFFFF/user.png" alt=""
+    /></a>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
