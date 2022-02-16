3aa1e825fedeb80bb89d0fc0b44d75bce8d6b737
Hahalley <m222817735@gmail.com>
2022-02-17 01:07:43 +0800

    add Bootstrape vue

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/tailwind.css'
+// BOotstrap-Vue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
