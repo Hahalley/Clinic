4159349b1e0951c3c209604a522455109d16702a
Hahalley <m222817735@gmail.com>
2022-02-17 15:40:05 +0800

    implement Resevation.vue

--- src/views/Resevation.vue
+++ src/views/Resevation.vue
@@ -7,6 +7,7 @@
         src="../assets/icon/triangle.png"
         alt=""
       />
+
       <!-- scroll -->
       <div class="tw-flex tw-mb-5 scroll">
         <div
@@ -48,7 +49,7 @@
       />
       <div>
         <select
-          class="tw-border tw-border-primary-400 tw-rounded-md tw-text-primary-400 tw-py-1 tw-px-20"
+          class="tw-border tw-border-primary-400 tw-rounded-md tw-text-primary-400 tw-py-1 tw-px-24 tw-mb-5"
         >
           <option>不指定醫師</option>
           <option>ＡＡＡ醫師</option>
@@ -56,28 +57,30 @@
           <option>ＣＣＣ醫師</option>
         </select>
       </div>
+      <!-- calendar -->
+      <div><h1>calendar</h1></div>
 
-      <!-- <div>
-        <b-form-select v-model="selected" :options="options"></b-form-select>
-        <b-form-select
-          v-model="selected"
-          :options="options"
-          size="sm"
-          class="mt-3"
-        ></b-form-select>
-        <div class="mt-3">
-          Selected: <strong>{{ selected }}</strong>
-        </div>
-      </div> -->
-      <!-- <select name="pets" id="pet-select">
-        <option value="">--Please choose an option--</option>
-        <option value="dog">Dog</option>
-        <option value="cat">Cat</option>
-        <option value="hamster">Hamster</option>
-        <option value="parrot">Parrot</option>
-        <option value="spider">Spider</option>
-        <option value="goldfish">Goldfish</option>
-      </select> -->
+      <span class="tw-text-primary-400">選擇看診時間</span>
+      <img
+        class="tw-opacity-40 tw-w-6 tw-h-6 tw-mb-2"
+        src="../assets/icon/triangle.png"
+        alt=""
+      />
+      <div>
+        <select
+          class="tw-border tw-border-primary-400 tw-rounded-md tw-text-primary-400 tw-py-1 tw-px-29 tw-mb-5"
+        >
+          <option>15:30</option>
+          <option>16:00</option>
+          <option>16:30</option>
+          <option>17:00</option>
+        </select>
+      </div>
+      <button
+        class="tw-text-white tw-text-sm tw-bg-primary-100 tw-border tw-border-primary-100 tw-rounded-md tw-px-32 tw-py-3 tw-mt-5"
+      >
+        下一步
+      </button>
     </div>
   </div>
 </template>
@@ -106,4 +109,5 @@ export default {
   width: 84vw;
   height: 50vw;
 }
+// calendar
 </style>
