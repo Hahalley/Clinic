99d0ba6be3fbf4b6b1f7870b232ab517bfba50b9
Hahalley <m222817735@gmail.com>
2022-02-20 16:40:37 +0800

    Rename resevation to reservation

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
-import Resevation from '../views/Resevation.vue'
+import Reservation from '../views/Reservation.vue'
 import Information from '../views/Information.vue'
 
 Vue.use(VueRouter)
@@ -19,9 +19,9 @@ const routes = [
     component: Home,
   },
   {
-    path: '/resevation',
-    name: 'Resevation',
-    component: Resevation,
+    path: '/reservation',
+    name: 'Reservation',
+    component: Reservation,
   },
   {
     path: '/information',
