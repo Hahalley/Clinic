d58aa1477f725edfb3eedaa47651ee6d1a785bcb
Hahalley <m222817735@gmail.com>
2022-02-20 16:39:20 +0800

    import v-calendar & vue-moment

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/tailwind.css'
-// BOotstrap-Vue
+// Bootstrap-Vue
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Third-party plugins
+import VCalendar from 'v-calendar'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VCalendar)
+Vue.use(require('vue-moment'))
 Vue.config.productionTip = false
 
 new Vue({
